c2acf966f23ca587943eba7166a029730253b911
Javsawe <alixankunaychbekov15@gmail.com>
2023-12-15 19:32:10 +0600

    Метод ввода чисел от пользователя

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-    }
-}
+        int num = sc.nextInt();
+        int num2 = sc.nextInt();
+
+        }
+        }
